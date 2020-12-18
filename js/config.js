a07582777a0d4fb5ab3580ec44a86b80f8b6a04a
Seb Carss <seb.carss@gmail.com>
2020-12-18 14:41:56 +0000

    add api integration

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-west-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://u9ddx5y8m1.execute-api.eu-west-1.amazonaws.com/prod'
     }
 };
