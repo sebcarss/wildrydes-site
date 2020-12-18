e27700b8075d8bd48e8610193ba1dea811f3884c
Seb Carss <seb.carss@gmail.com>
2020-12-18 14:09:50 +0000

    Add Cognito user pool intergration

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_qocXgUUha',
+        userPoolClientId: '6gctn0v36ks1095thd51nhqo55', 
+        region: 'eu-west-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
